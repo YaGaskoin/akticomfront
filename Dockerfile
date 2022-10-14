1a6d1cd7afd452d54ef4bdde8dfcba2a1b058168
danka <dchierntsov@mail.ru>
2022-10-14 13:47:45 +0300

    Updates

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM node:13-alpine
-EXPOSE 3000
-RUN mkdir /app
-WORKDIR /app/
-COPY ./package*.json ./
-RUN npm install
-COPY ./ /app
-CMD ['npm', 'start']
--- src/components/common/Preloader/Preloader.js
+++
@@ -1,9 +0,0 @@
- import loader from "../../../assets/images/ring.svg"
- import css from './Preloader.module.css'
- import React from "react";
-
- let Preloader = (props) => {
-     return <div className={css.preloader}><img className={css.preloader_img} src={loader}/></div>
-
- }
- export default Preloader
